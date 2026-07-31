41b59d7fad66166b89997cb22214d426bba79249
cong8191 <cong8191@gmail.com>
2026-07-31 21:00:39 +0700

    v

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.tracking.app',
-  appName: 'tracking game',
-  webDir: 'build'
-};
-
-export default config;
