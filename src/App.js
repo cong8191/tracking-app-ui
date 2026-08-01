c578aa3ef5c5931878a8d3377c1b0aaf3965dc6f
cong8191 <cong8191@gmail.com>
2026-08-01 09:57:19 +0700

    fsf

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, HashRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./MainPage";
 import UploadManager from "./UploadManager";
 import SQLiteFileUploader from "./SQLiteFileUploader";
@@ -9,14 +9,9 @@ import CreateNew from "./CreateNew";
 import MobileEditor from "./MobileEditor";
 import ViewImage from "./ViewImage";
 
-// Tự động chọn Router phù hợp với môi trường
-// @ts-ignore
-const isNative = window.Capacitor?.isNativePlatform();
-const AppRouter = isNative ? HashRouter : BrowserRouter;
-
 export default function App() {
   return (
-    <AppRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/upload" element={<UploadManager />} />
@@ -27,6 +22,6 @@ export default function App() {
         <Route path="/updateContent" element={<MobileEditor />} />
         <Route path="/vewImage/:event_id" element={<ViewImage />} />
       </Routes>
-    </AppRouter>
+    </HashRouter>
   );
 }
